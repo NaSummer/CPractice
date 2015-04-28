770eaa5adb63f24ff018d134e306623659d6334a
NaSummer <na_summer@icloud.com>
2015-04-28 22:16:12 +0800

    Creat my first C program
    Creat my first C program HelloWorld

---
+++ HelloWorld/src/HelloWorld.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : HelloWorld.c
+ Author      : Andy Xu
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	int i = 1;
+	printf("int i = %d", i);
+	return EXIT_SUCCESS;
+}
